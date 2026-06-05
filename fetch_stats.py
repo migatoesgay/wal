982327938d30e3f8a2c40720f77d51b0130ca0eb
migatoesgay <79529445+migatoesgay@users.noreply.github.com>
2026-06-05 09:59:49 +0200

    Update fetch_stats.py

--- fetch_stats.py
+++ fetch_stats.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-url = "https://solo.ckpool.org/users/bc1q4gp9ycevf88wz8nhej89m4n2w3s760h88845xl.json"
+url = "https://solo.ckpool.org/users/bc1q4gp9ycevf88wz8nhej89m4n2w3s760h88845xl"
 headers = {'User-Agent': 'Mozilla/5.0'}
 
 try:
