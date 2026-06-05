ff127508f4587b26979bdc641d36dc721bdfcca7
migatoesgay <79529445+migatoesgay@users.noreply.github.com>
2026-06-05 10:56:24 +0200

    Update fetch_stats.py

--- fetch_stats.py
+++ fetch_stats.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-# URL de tu billetera en CKPool
+# URL con .json al final para asegurar el formato de datos completo
 url = "https://solo.ckpool.org/users/bc1q4gp9ycevf88wz8nhej89m4n2w3s760h88845xl.json"
 headers = {'User-Agent': 'Mozilla/5.0'}
 
@@ -10,9 +10,9 @@ try:
     response = requests.get(url, headers=headers, timeout=10)
     if response.status_code == 200:
         data = response.json()
-        # Añadimos la fecha para el dashboard
         data["last_updated"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         
+        # Guardado limpio
         with open('stats.json', 'w') as f:
             json.dump(data, f, indent=4)
 except Exception as e:
